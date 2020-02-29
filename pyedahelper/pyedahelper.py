fa58af4da4041be583881135508a7346526b0622
Suvarna <suvarnamoharir@hotmail.com>
2020-02-28 16:07:50 -0800

    modifying docstrings for consistency

--- pyedahelper/pyedahelper.py
+++ pyedahelper/pyedahelper.py
@@ -1,19 +1,22 @@
-
 def fast_outlier_id(data, columns, method):
-    """Documentatation for function: fast_outlier_identifier
-
-    Analyzes the values of a given column list in a given dataframe, identifies outliers using either the Z-Score algorithm or interquantile range algorithm.
-    The return is a dataframe containing the following columns: column name, list containing the outlier's index position, percentage of total counts considered outliers.
+    """
+    The function takes in a dataframe and analyzes the values of a given column list, and 
+    identifies outliers using either the ZScore algorithm or interquantile range algorithm.
+    The return is a dataframe containing the following columns: column name, list 
+    containing the outlier's index position, percentage of total counts considered outliers.
 
     Arguments:
-    data (dataframe): Dataframe to be analyzed.
-    columns (list): List containing the columns to be analyzed with the Z-score algorithm.
-    method (string) : string indicating which method to be used to identify outliers (methods available are: "Z-score algorithm" or "Interquantile Range")
-
-
+    data (pandas dataframe) - dataframe to be analyzed.
+    columns (list) - the columns to be analyzed with the Z-score algorithm.
+    method (string) - which method to be used to identify outliers from: 
+    {"z-score algorithm", "interquartile range"} 
+    
     Returns:
-    dataframe: Results summary as a dataframe containing the following columns: column name, list containing the outlier's index position, percentaje of total counts considered outliers.
+    dataframe: Results summary as a dataframe containing the following columns: 
+    column name, list containingthe outlier's index position, percentage of total counts 
+    considered outliers.
     """
+    pass
 
 def fast_plot(df, x, y, plot_type):
     """
@@ -27,9 +30,8 @@ def fast_plot(df, x, y, plot_type):
     df - (pandas dataframe) the data that will be plotted
     x - (str) the column name for the x variable
     y - (str) the column name for the y variable
-    plot_type - (str) the type of plot from: {scatter, line, bar}
+    plot_type - (str) the type of plot from: {"scatter", "line", "bar"}
     """
-    
     pass
 
 
@@ -42,6 +44,7 @@ def fast_corr(df, col_name):
     df - (dataframe) the input data frame
     col_name - (list) the names of the columns selected for correlation analysis
     """
+    pass
     
 def fast_missing_impute(df, method, cols):
     """
