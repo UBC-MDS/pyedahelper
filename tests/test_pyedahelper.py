7fd11e43602fba345b7ec74483d175708c199e62
Ofer Mansour <mansour.ofer@gmail.com>
2020-03-03 15:04:22 -0800

    added test functions and set up readthedocs

--- tests/test_pyedahelper.py
+++ tests/test_pyedahelper.py
@@ -1,1 +1,15 @@
 from pyedahelper import pyedahelper
+
+
+def test_fast_outlier_id():
+    pass
+
+def test_fast_plot():
+    pass
+
+def test_fast_corr():
+    pass
+
+def test_fast_missing_impute():
+    pass
+
