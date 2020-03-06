93e471803163c83d9db076a635643dd55f72c748
Ofer Mansour <mansour.ofer@gmail.com>
2020-03-06 15:40:00 -0800

    updated for dependcies

--- tests/test_fast_plot.py
+++ tests/test_fast_plot.py
@@ -1,4 +1,6 @@
 import pytest
+import pandas as pd
+from pyedahelper import pyedahelper
 
 d = {"col_int": [1, 2, 3, 4, 5, 6], 
      "col_chr":["a", "b", "d", "d", "f","e"],
@@ -13,32 +15,32 @@ df
 
 def test_inputs():
     """
-    Function tests inputs for fast_plot function.
+    Function tests inputs for pyedahelper.fast_plot function.
     """
     # data input isn't data frame
     with pytest.raises(AssertionError, match = "Data must be in pandas Data Frame!"):
-        fast_plot(df = [1,2], x = "col_date", y = "col_int", plot_type = "line")
+        pyedahelper.fast_plot(df = [1,2], x = "col_date", y = "col_int", plot_type = "line")
     
     # x input isn't string
     with pytest.raises(AssertionError, match = "x column name must be a string!"):
-        fast_plot(df = df, x = ["col_date"], y = "col_int", plot_type = "line")
+        pyedahelper.fast_plot(df = df, x = ["col_date"], y = "col_int", plot_type = "line")
     
     # y input is not a column in data frame
     with pytest.raises(AssertionError, match = "y column name is not a column in data frame entered!"):
-        fast_plot(df = df, x = "col_date", y = "slkfjas", plot_type = "line")
+        pyedahelper.fast_plot(df = df, x = "col_date", y = "slkfjas", plot_type = "line")
     
     # plot type not bar/line/scatter
     with pytest.raises(AssertionError, match = 'plot_type must be either: "scatter", "line",  or "bar"'):
-        fast_plot(df = df, x = "col_date", y = "col_int", plot_type = "slfjalskf")
+        pyedahelper.fast_plot(df = df, x = "col_date", y = "col_int", plot_type = "slfjalskf")
     # x column should not be all null
     with pytest.raises(AssertionError, match = "x Column must not be all Null!"):
-        fast_plot(df = df, x = "col_nan", y = "col_int", plot_type = "line")
+        pyedahelper.fast_plot(df = df, x = "col_nan", y = "col_int", plot_type = "line")
 
 def test_scatter():
     """
-    Function tests scatter plots created by fast_plot function.
+    Function tests scatter plots created by pyedahelper.fast_plot function.
     """
-    a = fast_plot(df = df, x = "col_int", y = "col_flt", plot_type = "scatter")
+    a = pyedahelper.fast_plot(df = df, x = "col_int", y = "col_flt", plot_type = "scatter")
     
     assert a.height == 600
     assert a.width == 900
@@ -48,13 +50,13 @@ def test_scatter():
     
     # raise error if y is date
     with pytest.raises(Exception):
-        fast_plot(df = df, x = "col_int", y = "col_date", plot_type = "scatter")
+        pyedahelper.fast_plot(df = df, x = "col_int", y = "col_date", plot_type = "scatter")
         
 def test_line():
     """
-    Function tests line plots created by fast_plot function.
+    Function tests line plots created by pyedahelper.fast_plot function.
     """
-    a = fast_plot(df = df, x = "col_int", y = "col_flt", plot_type = "line")
+    a = pyedahelper.fast_plot(df = df, x = "col_int", y = "col_flt", plot_type = "line")
     
     assert a.height == 600
     assert a.width == 900
@@ -64,13 +66,13 @@ def test_line():
     
     # raise error if y is date
     with pytest.raises(Exception):
-        fast_plot(df = df, x = "col_int", y = "col_date", plot_type = "line")
+        pyedahelper.fast_plot(df = df, x = "col_int", y = "col_date", plot_type = "line")
 
 def test_bar():
     """
-    Function tests bar charts created by fast_plot function.
+    Function tests bar charts created by pyedahelper.fast_plot function.
     """
-    a = fast_plot(df = df, x = "col_int", y = "col_flt", plot_type = "bar")
+    a = pyedahelper.fast_plot(df = df, x = "col_int", y = "col_flt", plot_type = "bar")
     
     assert a.height == 600
     assert a.width == 900
@@ -80,10 +82,10 @@ def test_bar():
     
     # ensure Exception gets raised when both columns are non-numeric
     with pytest.raises(Exception):
-        fast_plot(df = df, x = "col_date", y = "col_date", plot_type = "bar")
+        pyedahelper.fast_plot(df = df, x = "col_date", y = "col_date", plot_type = "bar")
     with pytest.raises(Exception):
-        fast_plot(df = df, x = "col_chr", y = "col_chr", plot_type = "bar")
+        pyedahelper.fast_plot(df = df, x = "col_chr", y = "col_chr", plot_type = "bar")
     with pytest.raises(Exception):
-        fast_plot(df = df, x = "col_date", y = "col_chr", plot_type = "bar")
+        pyedahelper.fast_plot(df = df, x = "col_date", y = "col_chr", plot_type = "bar")
     with pytest.raises(Exception):
-        fast_plot(df = df, x = "col_chr", y = "col_date", plot_type = "bar")
+        pyedahelper.fast_plot(df = df, x = "col_chr", y = "col_date", plot_type = "bar")
